2947eae2966b235ca5a2e3d859e163e3ede2cf5a
Julien Greffe <jgreffe@cloudbees.com>
2025-07-29 16:52:29 +0200

    Check augmentation happens once
    Signed-off-by: Julien Greffe <jgreffe@cloudbees.com>

--- src/test/java/org/jenkinsci/plugins/workflow/steps/SynchronousNonBlockingStepExecutorServiceAugmentorTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/steps/SynchronousNonBlockingStepExecutorServiceAugmentorTest.java
@@ -1,11 +1,12 @@
 package org.jenkinsci.plugins.workflow.steps;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasItem;
 
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.hamcrest.Matchers;
+import org.junit.AfterClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -21,15 +22,40 @@ public class SynchronousNonBlockingStepExecutorServiceAugmentorTest {
     public LoggerRule loggerRule =
             new LoggerRule().record(AugmentorTestExtension.class, Level.FINE).capture(10);
 
+    /**
+     * As the JVM and classes are loaded only once for the whole test, {@link SynchronousNonBlockingStepExecution#getExecutorService()} augments only once. The current boolean keeps track of the augmentation status.
+     */
+    private static boolean augmented = false;
+
     @Test
-    public void smokes() throws Exception {
+    public void smokes_configured_once_A() throws Exception {
         SynchronousNonBlockingStepExecution.getExecutorService();
-        assertThat(loggerRule.getMessages(), hasItem("Augmenting"));
+        checkAugmentation();
+    }
+
+    @Test
+    public void smokes_configured_once_B() throws Exception {
+        SynchronousNonBlockingStepExecution.getExecutorService();
+        checkAugmentation();
+    }
+
+    private void checkAugmentation() {
+        if (augmented) {
+            assertThat(loggerRule.getMessages(), Matchers.emptyIterable());
+        } else {
+            assertThat(loggerRule.getMessages(), Matchers.hasItem("Augmenting"));
+            augmented = true;
+        }
+    }
+    
+    @AfterClass
+    public static void afterClass() {
+        // Reset the static state to ensure that the test can be run multiple times without issues.
+        assertThat(augmented, Matchers.is(true));
     }
 
     @TestExtension
-    public static class AugmentorTestExtension
-            implements SynchronousNonBlockingStepExecution.ExecutorServiceAugmentor {
+    public static class AugmentorTestExtension implements SynchronousNonBlockingStepExecution.ExecutorServiceAugmentor {
 
         private static final Logger LOGGER = Logger.getLogger(AugmentorTestExtension.class.getName());
 
